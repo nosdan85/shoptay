ed96b8062a9735eb9761e95ab5eb09796e366b56
NosMarket Dev <dev@nosmarket.com>
2026-05-17 23:59:25 -0700

    add http server for render

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,7 +1,8 @@
-﻿import { Client, GatewayIntentBits, ActivityType, Collection } from "discord.js";
+﻿import { Client, GatewayIntentBits, ActivityType } from "discord.js";
 import dotenv from "dotenv";
 import { handleInteraction } from "./handlers/interactionHandler";
 import { handleMessage } from "./handlers/messageHandler";
+import http from "http";
 
 dotenv.config();
 
@@ -28,3 +29,14 @@ client.on("messageCreate", async (message) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+// HTTP server cho Render
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running");
+});
+
+server.listen(PORT, () => {
+  console.log(`HTTP server listening on port ${PORT}`);
+});
